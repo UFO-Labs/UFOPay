947a738de03a497d09d9ad1958ffe31f88c684a6
A252dev <a252p@outlook.com>
2026-01-09 08:39:05 +0300

    Init

---
+++ UFO.ProfileApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UFO.ProfileApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new Dictionary<string, string> { { "key", "value" } });
+    }
+}
