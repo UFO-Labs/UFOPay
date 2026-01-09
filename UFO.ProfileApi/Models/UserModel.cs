58dc23544843773685e46d72ba4486689c221dfa
A252dev <a252p@outlook.com>
2026-01-09 13:30:17 +0300

    UserModel init

---
+++ UFO.ProfileApi/Models/UserModel.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UFO.ProfileApi.Models;
+
+[Table("users")]
+public class UserModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column(name: "username")]
+    public string Username { get; set; }
+
+    [Column(name: "password")]
+    public string Password { get; set; }
+
+    [Column(name: "email")]
+    public string Email { get; set; }
+
+    public UserModel()
+    {
+    }
+
+    public UserModel(int id, string username, string password, string email)
+    {
+        Id = id;
+        Username = username;
+        Password = password;
+        Email = email;
+    }
+}
